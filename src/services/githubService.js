20ae63a2701d8c505ecf8166655d64f3f6951e82
Milos <milos94su@gmail.com>
2023-07-17 12:51:28 +0200

    Github service and constants

---
+++ src/services/githubService.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { githubApiBaseUrl } from "../utils/constants";
+
+const githubApi = axios.create({
+  baseURL: githubApiBaseUrl,
+});
+
+export const getUserRepos = async (username) => {
+  try {
+    const response = await githubApi.get(`/users/${username}/repos`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user repos:", error);
+    throw error;
+  }
+};
+
+export const getUserReposPaginated = async (username, page, perPage) => {
+  try {
+    const response = await githubApi.get(
+      `/users/${username}/repos?page=${page}&per_page=${perPage}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user repos paginated:", error);
+    throw error;
+  }
+};
+
+export const getRepoDetails = async (username, repo) => {
+  try {
+    const response = await githubApi.get(`/repos/${username}/${repo}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching repo details:", error);
+    throw error;
+  }
+};
+
+export const getRepoTags = async (username, repo) => {
+  try {
+    const response = await githubApi.get(`/repos/${username}/${repo}/tags`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching repo tags:", error);
+    throw error;
+  }
+};
+
+export const getReleasesForRepo = async (username, repo) => {
+  try {
+    const response = await githubApi.get(`/repos/${username}/${repo}/releases`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching repo releases:", error);
+    throw error;
+  }
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const defaultRepoName = "octocat";
+export const githubApiBaseUrl = "https://api.github.com";
